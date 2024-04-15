569bb401fd4a1718c8ea30a529a2b796aa387db2
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-15 15:06:35 +0200

    Initial commit

---
+++ src/main/java/com/artemnizhnyk/springaitestproject/SpringAiTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.springaitestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemnizhnyk/springaitestproject/SpringAiTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.springaitestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
